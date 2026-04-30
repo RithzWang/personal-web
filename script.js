b6df266762469baa5e92fd23f6442d802a434e40
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-04-30 23:48:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -61,10 +61,10 @@ document.querySelectorAll('.flag-wrapper').forEach(wrapper => {
         // 3. Show the tooltip for the flag we just clicked
         this.classList.add('show-tooltip');
         
-        // 4. Automatically hide the tooltip after 2 seconds
+        // 4. Automatically hide the tooltip after 3 seconds
         setTimeout(() => {
             this.classList.remove('show-tooltip');
-        }, 2000); 
+        }, 3000); 
     });
 });
 
